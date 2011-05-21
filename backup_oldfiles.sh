b019f21520074487481ad3341ca56b15481ea463
Bernhard Eder <git@bbgen.net>
2011-05-21 12:34:25 +0200

    added backup file.

---
+++ backup_oldfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# "THE MODIFIED BEER-WARE LICENSE"
+# <git@bbgen.net> wrote this file. You can do whatever you want with this stuff.
+# If we meet some day, and you think this stuff is worth it, you can buy me a
+# beer in return Bernhard Eder
+# ----------------------------------------------------------------------------
+
+mkdir old
+mv ~/.bashrc old
+mv ~/.vimrc old
+mv ~/.screenrc old
+mv -r ~/.vim old
+mv ~/.gitconfig old
+
